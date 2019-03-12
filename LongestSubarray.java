69415c9f895e1f7f4b478bfcc109039a61fac754
Shailesh Kumar <Shailesh8@users.noreply.github.com>
2019-03-13 04:11:13 +0530

    create return type

--- LongestSubarray.java
+++ LongestSubarray.java
@@ -1,17 +1,61 @@
+
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 // create custom exception for array of length =0 
-class LengthNotFoundException extends Exception {
-	public LengthNotFoundException(String message) {
+class NullArrayException extends Exception {
+	public NullArrayException(String message) {
 		super(message);
+	}
+}
+
+class ResultObject {
+	int lowerIndex;
+	int maxLengthSubArray;
+	int maxSum;
+	int[] arr;
+
+	ResultObject(int lowerIndex, int maxLengthSubArray, int maxSum, int[] arr) {
+		this.lowerIndex = lowerIndex;
+		this.maxLengthSubArray = maxLengthSubArray;
+		this.maxSum = maxSum;
+		this.arr = arr;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
+		if (!ResultObject.class.isAssignableFrom(obj.getClass()))
+			return false;
+		final ResultObject other = (ResultObject) obj;
+		if (this.lowerIndex != other.lowerIndex)
+			return false;
+		if (this.maxLengthSubArray != other.maxLengthSubArray)
+			return false;
+		if (this.maxSum != other.maxSum)
+			return false;
+		if (this.arr == null) {
+			if (other.arr != null)
+				return false;
+		}
+		for (int i = 0; i < other.arr.length; i++) {
+			if (this.arr[i] != other.arr[i])
+				return false;
+		}
+		return true;
 	}
 }
 
 public class LongestSubarray {
 
 	// find length of longest subarray with maximum sum
-	private static void lengthLongestSubArrayWithMaxSum(int arr[], int arrSize) {
+	public static ResultObject lengthLongestSubArrayWithMaxSum(int arr[]) throws NullArrayException {
+		int arrSize = arr.length;
+		if(arrSize == 0){
+			throw new NullArrayException("Given array is empty");
+		}
+		
 		int lower_limit = 0;
 		int max_sum = 0;
 		int sum = 0;
@@ -54,26 +98,38 @@ public class LongestSubarray {
 		System.out.println("Length: " + maxLengthSubArray);
 		System.out.println("Sum: " + max_sum);
 		System.out.print("Elements: ");
+		int[] brr = new int[maxLengthSubArray];
+		int count = 0;
 		for (int i = max_lower_index; i < max_lower_index + maxLengthSubArray; i++) {
 			System.out.print(arr[i] + " ");
+			brr[count++] = arr[i];
 		}
+
+		ResultObject resultObject = new ResultObject(max_lower_index, maxLengthSubArray, max_sum, brr);
+		return resultObject;
+
 	}
 
-	public static void main(String[] args) throws LengthNotFoundException {
-
+	public static String getString() {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.nextLine();
+		return str;
+	}
+
+	public static void main(String[] args) throws NullArrayException {
+
+		String str = getString();
 		StringTokenizer stringTokenizer = new StringTokenizer(str, " ");
-		if (stringTokenizer.countTokens() == 0) {
-			throw new LengthNotFoundException(" Given array is empty");
-		}
+//		if (stringTokenizer.countTokens() == 0) {
+//			throw new NullArrayException(" Given array is empty");
+//		}
 		int sizeOfArray = stringTokenizer.countTokens();
 		int[] arr = new int[sizeOfArray];
 		int i = 0;
 		while (stringTokenizer.hasMoreTokens()) {
 			arr[i++] = Integer.parseInt(stringTokenizer.nextToken());
 		}
-		lengthLongestSubArrayWithMaxSum(arr, sizeOfArray);
+		lengthLongestSubArrayWithMaxSum(arr);
 	}
 
 }
